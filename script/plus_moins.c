21eb5fd23cf871a27f9b0ab7b31e5f09667a85b8
DeveshKB <dkb020102@gmail.com>
2026-03-10 14:42:59 +0400

    2eme  commit

--- script/plus_moins.c
+++ script/plus_moins.c
@@ -1,14 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void plus_moins(int choix_joueur, int valeur_a_trouver)
-{
-	if(choix_joueur < valeur_a_trouver) {
-		printf("plus\n");
-	}
-	else if (choix_joueur > valeur_a_trouver) {
-		printf("moins\n");
-	}
-	else {
-		printf("Bingo\n");
-	}
+void plus_moins(int choix_joueur, int valeur_a_trouver) {
+    if (choix_joueur < valeur_a_trouver) {
+        printf("plus\n");
+    }
+    else if (choix_joueur > valeur_a_trouver) {
+        printf("moins\n");
+    }
+    else {
+        printf("Bingo\n");
+    }
+}
+
+int main() {
+
+    int choix_joueur;
+    int valeur_a_trouver = 42; // valeur choisie
+
+    do {
+        printf("Choisissez un nombre : ");
+        scanf("%d", &choix_joueur);
+
+        plus_moins(choix_joueur, valeur_a_trouver);
+
+    } while (choix_joueur != valeur_a_trouver);
+
+    return 0;
 }
